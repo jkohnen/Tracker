6ff4555d813c1c6a3e7e5a2a61e321cb52844af6
libra guest <jkfilter@gmail.com>
2012-07-26 16:45:41 -0400

    Moving the Master page to a data attribute

--- source/Tracker.PhoneGap/www/bootstrap.js
+++ source/Tracker.PhoneGap/www/bootstrap.js
@@ -1,35 +1,37 @@
-﻿var serviceUri = "http://localhost:56146/data.txt"
+var serviceUri = "https://c9.io/jkohnen/tracker/workspace/source/Tracker.Web/data.txt";
 
 function loadcssfile(filename) {
 	
-		var fileref = document.createElement("link")
-		fileref.setAttribute("rel", "stylesheet")
-		fileref.setAttribute("type", "text/css")
-		fileref.setAttribute("href", filename)
+		var fileref = document.createElement("link");
+		fileref.setAttribute("rel", "stylesheet");
+		fileref.setAttribute("type", "text/css");
+		fileref.setAttribute("href", filename);
 		if (typeof fileref != "undefined")
-			document.getElementsByTagName("head")[0].appendChild(fileref)
+        {
+			document.getElementsByTagName("head")[0].appendChild(fileref);
+        }
 }
 
 function loadjsfile(filename) {
-		var fileref = document.createElement('script')
-		fileref.setAttribute("type", "text/javascript")
-		fileref.setAttribute("src", filename)
+		var fileref = document.createElement('script');
+		fileref.setAttribute("type", "text/javascript");
+		fileref.setAttribute("src", filename);
 		if (typeof fileref != "undefined")
-			document.getElementsByTagName("head")[0].appendChild(fileref)
+        {
+			document.getElementsByTagName("head")[0].appendChild(fileref);
+        }
 }
 
-// In the header of your page, paste the following for Kendo UI Mobile styles
 loadcssfile("styles/kendo.mobile.all.min.css");
-
 loadjsfile("cordova-1.8.0.js");
 loadjsfile("js/kendo.mobile.min.js");
-//loadjsfile("js/kendo.all.min.js");
 
 function onDeviceReady() {
+    var masterPage = $('html').data('masterpage');
 	if (masterPage) {
 		$.get(masterPage, function (template) {
 			$("body").append(template);
-		})
+		});
 	}
 
 }
